3b16330d5e977676fa0eb7d37a58ca73268123f9
Anon <Anon>
2023-06-14 23:38:36 +0200

    Commenting out all of supabaseClient details in the comment

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/auth-helpers-sveltekit'
-import { env } from "$env/dynamic/public";
+// In the version "@supabase/auth-helpers-sveltekit": "^0.10.1" the createClient doesnt exist anymore.
+// import { createClient } from '@supabase/auth-helpers-sveltekit'
+// import { env } from "$env/dynamic/public";
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+// export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
